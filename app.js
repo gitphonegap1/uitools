504cf59fa6773078ff58fdced8918c1d46cd3881
gitphonegap1 <ganti143@gmail.com>
2021-07-12 16:46:06 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+/* eslint-disable */
+const express = require('express');
+const PORT = 3000;
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+app.get('/hello',(req,res,next)=>{
+    res.send('Welcome to uitools_services with firebase');
+})
+
+
+app.listen(PORT,()=>{
+    console.info('Server is running on the PORT :: '+PORT);
+})
+
